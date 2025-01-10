434d9dccbb5ee850edf59ead135651723a3dd876
merpassenger <yollcumerve@gmail.com>
2025-01-11 02:27:03 +0300

    async programming, task class's ContinueWith method

--- async-multithread-programming/Controllers/HomeController.cs
+++ async-multithread-programming/Controllers/HomeController.cs
@@ -4,12 +4,13 @@ using Microsoft.AspNetCore.Mvc;
 namespace async_multithread_programming.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]
-public class HomeController : Controller
+[Route("[controller]")]
+public class HomeController : ControllerBase
 {
-    
-    //Asenkron programlamada Task sınıfı ile geriye bir durum kodu dönüleceğinin sözü verilmekte ve işlem başlamakta, işlem bitene kadar bir bekleme veya kitlenme söz konusu değildir.
-    //Bu metot devam ederken başka istekler alınabilmektedir
+    /*
+    Asenkron programlamada Task sınıfı ile geriye bir durum kodu dönüleceğinin sözü verilmekte ve işlem başlamakta, işlem bitene kadar bir bekleme veya kitlenme söz konusu değildir.
+    Bu metot devam ederken başka istekler alınabilmektedir
+    */
     [HttpGet("GetContentAsync")]
     public async Task<IActionResult> GetContentWithTask()
     {
@@ -19,16 +20,51 @@ public class HomeController : Controller
 
         return Ok(data.Length);
     }
-    
-    //Burada senkron bir metot olduğundan içeride thread tarafından metot çağrıldığında burada Ok ile data dönülünceye kadar başka bir istğin yapılması engellenecektir.
+
+    /*
+     * Burada senkron bir metot olduğundan içeride thread tarafından metot çağrıldığında burada Ok ile data dönülünceye kadar başka bir istğin yapılması engellenecektir.
+     */
     [HttpGet("GetContent")]
     public IActionResult GetContentWithoutTask()
     {
         var client = new WebClient();
-        var task =  client.DownloadString("https://www.google.com");
+        var task = client.DownloadString("https://www.google.com");
 
         var data = task;
 
         return Ok(data.Length);
+    }
+
+    /*
+     * Her async method async-await ikilisine sahip olmak zorunda değildir. Async, metot içerisinde async method kullanılacaksa gereklidir.
+     * Bir metot async-await ikisilisi olmadan da text dönebileceğini gösterir.
+     * Burada metot benden Task istemekte ve kullandığım async metot da geriye bir Task return etmekte olduğundan metot içerisinde bir patlama olmadı
+     */
+    [HttpGet("GetFileText")]
+    public Task<string> ReadFileAsync()
+    {
+        var s = new StreamReader("file.txt");
+        return s.ReadToEndAsync();
+    }
+    
+    [HttpGet("GetDataTaskContinueWith")]
+    public async Task<IActionResult> GetDataTaskContinueWithAsync()
+    {
+        Console.WriteLine("etc1");
+
+        var task = new HttpClient().GetStringAsync("https://www.google.com")
+            .ContinueWith(data => { Console.WriteLine(data.Result.Length); });
+
+        Console.WriteLine("etc2");
+
+        await task;
+
+        return Ok(task);
+        
+        /*
+         * etc1
+         * etc2
+         * task data 
+         */
     }
 }
